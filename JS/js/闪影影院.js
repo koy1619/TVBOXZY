1038a84b9fb910af5d6693260ca6b48b7dd47ef3
zhanghong1983 <108555567+zhanghong1983@users.noreply.github.com>
2022-11-30 10:56:38 +0800

    Add files via upload

--- JS/js/闪影影院.js
+++ JS/js/闪影影院.js
@@ -1,6 +1,6 @@
 var rule = {
-    title:'起飞影院',
-    host:'http://www.qfitv.com',
+    title:'闪影影院',
+    host:'https://sytv.cc',
     // homeUrl:'/',
     url:'/index.php/vod/show/id/fyclass/page/fypage.html',
     searchUrl:'/index.php/vod/search/page/fypage/wd/**.html',
@@ -11,8 +11,8 @@ var rule = {
         'User-Agent':'MOBILE_UA',
         // "Cookie": "searchneed=ok"
     },
-    class_name:'电影&电视剧&综艺&动漫&纪录片&喜剧片&爱情片&同性片&剧情片&国产剧&欧美剧&泰剧',
-    class_url:'1&2&3&4&55&7&8&23&11&13&16&33',
+    class_name:'电影&电视剧&综艺&动漫&纪录片&国产&港台&日韩&欧美',
+    class_url:'1&2&3&4&27&13&14&15&16',
     play_parse:true,
     lazy:'',
     limit:6,
@@ -22,4 +22,3 @@ var rule = {
     二级:{"title":"h1&&Text;.module-info-tag-link:eq(2)&&Text","img":".ls-is-cached.lazy.lazyload&&data-original","desc":".module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(2)&&.module-info-item-content&&Text;.module-info-content&&.module-info-item:eq(1)&&.module-info-item-content&&Text","content":".module-info-introduction-content&&Text","tabs":".module-tab-items-box:eq(0)&&.module-tab-item","lists":".module-play-list-content:eq(0) a"},
     搜索:'.module-card-item.module-item;.module-card-item-title&&Text;img&&data-original;.module-item-note&&Text;a.play-btn-o&&href',
 }
-
